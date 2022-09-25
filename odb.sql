a80343003464158b6f1b890f0130b1a315945073
Nelson Chen <crazysim@gmail.com>
2022-09-24 20:43:45 -0700

    Use  static.wiki

--- odb.sql
+++ odb.sql
@@ -1,10 +1,17 @@
+PRAGMA journal_mode = OFF;
+PRAGMA synchronous = 0;
+pragma page_size = 8192;
+PRAGMA cache_size = 250000;
+PRAGMA locking_mode = EXCLUSIVE;
+PRAGMA temp_store = MEMORY;
+
 CREATE VIRTUAL TABLE records USING fts5(
   "PROPERTY_ID",
-  "PROPERTY_TYPE",
-  "CASH_REPORTED",
-  "SHARES_REPORTED",
+  "PROPERTY_TYPE" UNINDEXED,
+  "CASH_REPORTED" UNINDEXED,
+  "SHARES_REPORTED" UNINDEXED,
   "NAME_OF_SECURITIES_REPORTED",
-  "DATE_REPORTED",
+  "DATE_REPORTED" UNINDEXED,
   "NO_OF_OWNERS" UNINDEXED,
   "OWNER_NAME",
   "OWNER_STREET_1",
@@ -15,9 +22,9 @@ CREATE VIRTUAL TABLE records USING fts5(
   "OWNER_ZIP",
   "OWNER_COUNTRY_CODE",
   "CURRENT_CASH_BALANCE",
-  "NUMBER_OF_PENDING_CLAIMS",
-  "NUMBER_OF_PAID_CLAIMS",
-  "DATE_OF_LAST_CONTACT",
+  "NUMBER_OF_PENDING_CLAIMS" UNINDEXED,
+  "NUMBER_OF_PAID_CLAIMS" UNINDEXED,
+  "DATE_OF_LAST_CONTACT" UNINDEXED,
   "HOLDER_NAME",
   "HOLDER_STREET_1",
   "HOLDER_STREET_2",
@@ -25,8 +32,15 @@ CREATE VIRTUAL TABLE records USING fts5(
   "HOLDER_CITY",
   "HOLDER_STATE",
   "HOLDER_ZIP",
-  "CUSIP"
+  "CUSIP",
+  tokenize="porter ascii"
 );
 .mode csv
 .import 00_All_Records/All_Records__File_1_of_1.csv records
--- INSERT INTO records(records) VALUES('optimize');
+
+-- https://github.com/segfall/static-wiki/blob/master/scripts/optimize_db.js
+INSERT INTO records(records, rank) VALUES ('rank', 'bm25(1, 10)');
+INSERT INTO records(records) VALUES ('optimize');
+
+VACUUM;
+ANALYZE;
